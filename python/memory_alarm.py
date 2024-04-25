c8b2a04287e02ba66305414d3f26528a947a1be6
Alexey Krasichonak <alexey.krasich@gmail.com>
2024-04-25 19:53:15 +0300

    Script for checking memory usage and making alerts in tg api. Working in fg, need to be fixed.

--- python/memory_alarm.py
+++ python/memory_alarm.py
@@ -1,1 +1,36 @@
-1
+###----------------------------------------------------
+#-Owner: Alexey Krasichonak
+#-Discription: Send an http alarm to telegram bot via API if memory usage is greater than 80%
+###----------------------
+
+import time
+import psutil
+import requests
+
+def send_tg_msg(bot_token, chat_id, message):
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    data = {
+        "chat_id": chat_id,
+        "text": message
+    }
+    response = requests.post(url, data=data)
+    if response.status_code == 200:
+        print("Message was sent.")
+    else:
+        print("Can't send a message:", response.text)
+
+def check_memory():
+    while True:
+
+        memory_usage = psutil.virtual_memory().percent
+
+        if memory_usage > 80:
+            bot_token = "###YOUR_TG_TOKEN###"
+            chat_id = "###YOUR_CHAT_ID###"
+            message = "ALARM! Memory usage is more than 80%: {}%".format(memory_usage)
+            send_tg_msg(bot_token, chat_id, message)
+
+        time.sleep(60)
+
+if __name__ == "__main__":
+    check_memory()
